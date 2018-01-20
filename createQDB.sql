a5e450f57c7c7035cd7c978f82dbb74ebf191187
1nF0rmed <prkinformed@gmail.com>
2018-01-20 14:06:51 +0530

    Basic

---
+++ createQDB.sql
@@ -0,0 +1,20 @@
+CREATE TABLE question(
+  hash varchar(250),
+
+);
+
+// what we need
+1) question title
+2) question content 
+3) test cases
+
+
+test cases
+-> hash
+-> id
+-> function name
+-> params (php string array or something like that)
+-> i/o (php string array or something like that)
+-> output (php array)
+
+primary key -> unique values
